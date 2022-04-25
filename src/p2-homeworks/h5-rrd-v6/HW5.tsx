f2b276842f9ca6af2255e1301c07307f04d0c760
Ilya Khorobrykh <horobryh17@gmail.com>
2022-04-25 13:43:13 +0300

    Finished 5th Homework

--- src/p2-homeworks/h5-rrd-v6/HW5.tsx
+++ src/p2-homeworks/h5-rrd-v6/HW5.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from './Header'
 import Pages from './Pages'
-import {BrowserRouter, HashRouter} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 import classes from '../../p1-main/m1-ui/u1-app/App.module.css';
 
 function HW5() {
--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, {MouseEvent, useState} from 'react';
 import {NavLink} from 'react-router-dom';
+import classes from './../../p1-main/m1-ui/u1-app/App.module.css'
 
 function Header() {
+
+    const [collapsed, setCollapsed] = useState<boolean>(true);
+    const navClassName = collapsed ? classes.sidebar + ' ' + classes.sidebarClose : classes.sidebar;
+
+    const onMouseEnterHandler = (e: MouseEvent<HTMLElement>) => {
+        setCollapsed(!collapsed);
+    }
+
+    const onMouseLeaveHandler = (e: MouseEvent<HTMLElement>) => {
+        if (!collapsed) setCollapsed(!collapsed);
+    }
+
     return (
-        <div>
-            <NavLink to={'/react-tasks-from-ignat/pre-junior'}>Pre-Junior</NavLink>
-            <NavLink to={'/react-tasks-from-ignat/junior'}>Junior</NavLink>
-            <NavLink to={'/react-tasks-from-ignat/junior-plus'}>Junior-Plus</NavLink>
-        </div>
+        <nav onMouseLeave={onMouseLeaveHandler} className={navClassName}>
+            <header>
+                <div className={classes.text + ' ' + classes.logoText}>
+                    <NavLink
+                        className={({isActive}) => isActive ? classes.linkActive : classes.link}
+                        to={'/react-tasks-from-ignat/pre-junior'}>Pre-Junior</NavLink>
+                    <NavLink
+                        className={({isActive}) => isActive ? classes.linkActive : classes.link}
+                        to={'/react-tasks-from-ignat/junior'}>Junior</NavLink>
+                    <NavLink
+                        className={({isActive}) => isActive ? classes.linkActive : classes.link}
+                        to={'/react-tasks-from-ignat/junior-plus'}>Junior-Plus</NavLink>
+                </div>
+
+                <div onMouseEnter={onMouseEnterHandler} className={`bx bx-chevron-right ${classes.toggle}`}/>
+            </header>
+        </nav>
     )
 }
 
